7427ffe510b72ceb012660b54af16a68f61dec50
Ismael Alvarez <ismaelalvarez514@gmail.com>
2024-04-30 12:28:55 -0600

    Initial proyect setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React,{Fragment} from "react"
+
+
+const  App:React.FC =()=> {
+
+
+  return (
+    <Fragment>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </Fragment>
+  )
+}
+
+export default App
